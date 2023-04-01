a86a79d637eebc101f63ca04ed8a32a09e047acc
colburncodes <colburnsanders@gmail.com>
2023-03-31 21:30:11 -0500

    file structure and routes

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const path = require("path");
+const usersRouter = require("./routes/users");
+const cardsRouter = require("./routes/cards");
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", usersRouter);
+app.use("/", cardsRouter);
 
 app.listen(PORT, () => {
   console.info(`
