0d840bfb93c5b3f55cae6a81ae9f42da641b6628
colburncodes <colburnsanders@gmail.com>
2023-04-08 09:22:23 -0500

    refactor: :recycle: refactor card api

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const routes = require("./routes/index");
 const app = express();
 
 app.use(express.json());
-app.use("/", routes);
-
 app.use((req, res, next) => {
   req.user = {
-    _id: "5d8b8592978f8bd833ca8133",
+    _id: "643155e2fa60a0727aa0e603",
   };
 
   next();
 });
+
+app.use("/", routes);
+
+
 
 module.exports = app;
