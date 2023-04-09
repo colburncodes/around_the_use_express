dd5646b2fabfc7c67523bd1c0c5e7af84562f92c
colburncodes <colburnsanders@gmail.com>
2023-04-09 11:51:11 -0500

    refactor: :recycle: update routes and error handling

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,15 @@
 const User = require("../models/user");
+const Status = require("../utils/error");
 
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => {
-      res.status(200).send({ users });
+      res.status(Status.STATUS_CODES.Ok).send({ users });
     })
-    .catch((error) => {
-      res.status(500).send({ message: "Error making request" });
+    .catch(() => {
+      res
+        .status(Status.STATUS_CODES.ServerError)
+        .send({ message: "Error making request" });
     });
 };
 
@@ -14,10 +17,22 @@ const getUser = async (req, res) => {
   const { id } = req.params;
   User.findById(id)
     .then((user) => {
-      res.status(200).send({ user });
+      res.status(Status.STATUS_CODES.Ok).send({ user });
     })
-    .catch(() => {
-      res.status(500).send({ message: "User does not exist" });
+    .catch((error) => {
+      if (error.name === "NotFound") {
+        res
+          .status(Status.STATUS_CODES.BadRequest)
+          .send({ message: "Invalid User ID" });
+      }
+      if (error.name === "DocumentNotFoundError") {
+        res
+          .status(Status.STATUS_CODES.NotFound)
+          .send({ message: "User Not Found" });
+      }
+      res
+        .status(Status.STATUS_CODES.ServerError)
+        .send({ message: "User does not exist" });
     });
 };
 
@@ -25,25 +40,62 @@ const createUser = (req, res, next) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => {
-      res.status(201).send({
+      res.status(Status.STATUS_CODES.Created).send({
         user,
       });
     })
-    .catch(() => {
-      res.status(500).send({ message: "Error creating user" });
+    .catch((error) => {
+      if (error.name === "ValidationError") {
+        res
+          .status(Status.STATUS_CODES.BadRequest)
+          .send({ message: error.message });
+      } else {
+        res
+          .status(Status.STATUS_CODES.ServerError)
+          .send({ message: "Error creating user" });
+      }
     });
 };
 
-const updateUser = (req, res, next) => {
+const updateProfile = (req, res, next) => {
   const { id } = req.params;
+  const { name, about } = req.body;
 
-  const { name, about, avatar } = req.body;
-  User.findByIdAndUpdate(id, { $set: { name, about, avatar } })
+  User.findByIdAndUpdate(id, { $set: { name, about } })
     .then((user) => {
-      res.status(200).send({ user });
+      res.status(Status.STATUS_CODES.Ok).send({ user });
     })
     .catch((error) => {
-      res.status(500).send({ message: "Error updating user", error });
+      if (error.name === "ValidationError") {
+        res
+          .status(Status.STATUS_CODES.BadRequest)
+          .send({ message: error.message });
+      } else {
+        res
+          .status(Status.STATUS_CODES.ServerError)
+          .send({ message: "Error updating user", error });
+      }
     });
 };
-module.exports = { getUsers, getUser, createUser, updateUser };
+
+const updateAvatar = (req, res, next) => {
+  const { id } = req.params;
+  const { avatar } = req.body;
+
+  User.findByIdAndUpdate(id, { $set: { avatar } })
+    .then((user) => {
+      res.status(Status.STATUS_CODES.Ok).send({ user });
+    })
+    .catch((error) => {
+      if (error.name === "ValidationError") {
+        res
+          .status(Status.STATUS_CODES.BadRequest)
+          .send({ message: error.message });
+      } else {
+        res
+          .status(Status.STATUS_CODES.ServerError)
+          .send({ message: "Error updating user", error });
+      }
+    });
+};
+module.exports = { getUsers, getUser, createUser, updateProfile, updateAvatar };
