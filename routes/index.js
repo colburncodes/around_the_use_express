8ccd3c7936901c8a22c7c8014089a2a8d31a65e5
colburncodes <colburnsanders@gmail.com>
2023-04-12 20:31:46 -0500

    refactor: :recycle: refactor request, issue with user auth

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 const router = require("express").Router();
+const auth = require("../middlewares/auth");
+
+const { login, createUser } = require("../controllers/users");
 
 const usersRouter = require("./users");
 const cardsRouter = require("./cards");
 
+router.post("/signin", login);
+router.post("/signup", createUser);
+
 router.use("/users", usersRouter);
 router.use("/cards", cardsRouter);
 
-router.use((req, res) => {
+router.use(auth, (req, res) => {
   res.status(400).send({
     message: "No page found for route",
   });
---
+++ test/user-endpoint.test.js
@@ -0,0 +1,14 @@
+// endpoint.test.js
+const supertest = require("supertest");
+const app = require("./app.js");
+
+const request = supertest(app);
+
+// describe("Endpoints respond to requests", () => {
+//   it('Returns data and status 200 on request to "/"', () => {
+//     return request.get("/users").then((response) => {
+//       expect(response.status).toBe(200);
+//       // expect(response.text).toBe('Hello, world!');
+//     });
+//   });
+// });
